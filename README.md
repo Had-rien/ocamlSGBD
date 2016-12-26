0bd4c35a9d576843ea967ecb5cd2ae5f82cd2f8f
foucault2015 <hadf03@yahoo.fr>
2016-12-26 14:07:54 +0100

    fonction tmap working, allowing to apply a function to a line of our bdd

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+
 # ocamlSGBD
-# ocamlSGBD
+according to 
+http://www.ensiie.fr/~forest/IPF/projet/ipf_projet_2016.html
--- db.ml
+++ db.ml
@@ -30,8 +30,6 @@ type constraint_type =
 *)
 (* print a string contained in a Value *)
 let printStr i = match i with
-	| None -> ()
-	| Some x -> match x with
 		| Null -> ()
 		| Value (n) -> print_string n
 ;;
@@ -48,15 +46,6 @@ let printInt i = match i with
 		| Value (n) -> print_int n
 ;;
 
-
-(* type for lines witc h is a collection of line *)
-type lines = string list
-;;
-
- (* type of our table  it contains the description (schema), the data itsself in lines and constraint with constraint_type *)
-type table = { s : schema; l : lines; c : constraint_type}
-;;
-
 (* Use of trie structure to represent each line, for each line of one table a trie is created *)
 (* Use of module Map to easily create our trie *)
 module CMap = Map.Make(Char)
@@ -64,48 +53,124 @@ module CMap = Map.Make(Char)
 
 (* our trie contains data using option type to easily reuse it 
 In this project  we'll put char in each node (because our column name are string) *)
-type 'a trie = Trie of ('a option * 'a trie CMap.t)
+type 'a trie = Trie of ('a value * 'a trie CMap.t)
 ;;
 
 (* an empty tree is a trie with no cata pointing to an empty Map *)
-let emptyTrie = Trie (None, CMap.empty);;
+let emptyTrie = Trie (Null, CMap.empty);;
 
 (* addToLine allow to add an element in our trie in the node coresponding to the last char of str *)
 (* 	@requires str might be empty but not null, the trie must exist but might be emptyTrie, ele
 	@ensures that the element to be added (ele) is in the right place inside the trie given
 	@raises nothing for now, should warn in the futur if add an element has erased another one 
 *)
-let rec addToLine str ele (Trie (value,tList)) = let len = ((String.length str) - 1) in match len with
-	| 0 -> Trie (Some (Value ele), tList)
-	| _ -> let child = try ( CMap.find (String.get str 0) tList )
+let rec addToLine str ele (Trie (value,tList)) = let len = ((String.length str) - 0) in match len with
+	| 0 -> Trie ( ele, tList) (* end of str, we put ele inside the node *)
+	| _ -> let child = try ( CMap.find (String.get str 0) tList ) (* test if first char of str is in the map of this node *)
 		with
-			| Not_found -> emptyTrie
-		in Trie ( value, (CMap.add (String.get str 0) (addToLine (String.sub str 1 len) ele child) tList))
+			| Not_found -> emptyTrie (* if not we create a new node *)
+		(* eather way we create a new tree replacing the one in argument with the right parametersne
+		*)
+		in Trie ( value, (CMap.add (String.get str 0) (addToLine (String.sub str 1 (len-1)) ele child) tList))
 ;;
+
+(*
+	@ requires a trie that is not null but might be emptyTrie
+	@ ensures to return the value in the trie
+	@ raises
+*)
+let getValue (Trie (value,tList)) = value;;
+
 (*
 	@ requires a trie containing a valid map
 	@ ensures to return the value at str inside the trie if it exist ( the value or str in the trie)
 	@ raises nothing now, should warn if str not in trie
 *)
 let rec findInLine str (Trie (value,tList) as cur) = let len = ((String.length str) - 1) in match len with 
-	| 0 -> cur
+	| 0 -> getValue cur
 	| _ -> let child = try ( CMap.find (String.get str 0) tList )
 		with
 			| Not_found -> emptyTrie
 		in findInLine (String.sub str 1 len) child
 ;;
+
 (*
 	@ requires a trie that is not null but might be emptyTrie
-	@ ensures to return the value in the trie
+	@ ensures to print a representation of the trie, not pretty but allow to understand the representation
 	@ raises
 *)
+let rec printTrie c (Trie (value,tList)) = 
+	print_string "["; print_char c; print_string ", ";
+	match tList = CMap.empty with
+		| true -> printStr value; print_string "]";  print_newline ();
+		| _ -> printStr value; print_string "]"; (CMap.iter printTrie tList);
+;;
 
+(*
+	@ requires a trie that is not null but might be emptyTrie, f is a function that takes a Value and return one
+	@ ensures to apply f to all values of the given trie and return it
+	@ raises
+*)
+let rec tmap f (Trie (value,tList)) = 
+	match tList = CMap.empty with
+		| true -> Trie ((f value), tList)
+		| _ -> Trie ((f value), (CMap.map (tmap f) tList))
+;;
+
+(* 
+ 	@requires str might be empty but not null, the trie must exist but might be emptyTrie, ele
+	@ ensures to call addToLine only if we insert a not empty string
+	@ raises
+*)
+let checkAddTrie str ele trie = match ele with
+	| "" -> trie
+	| _ -> addToLine str ((Value ele)) trie
+;;
+
+(*----------end of line representation functions ------*)
+
+(* type for lines witch is a collection of line *)
+type 'a lines = ('a value) list
+;;
+
+ (* type of our table  it contains the description (schema), the data itsself in lines and constraint with constraint_type *)
+type 'a table = { s : schema; l : ('a value) lines; c : constraint_type}
+;;
+
+
+let addLine newL lineList = newL::lineList
+;;
+
+(*----------TEST----------------*)
 (* test of addToLine and findInLine function *)
-let getValue (Trie (value,tList)) = value;;
 
-let test = addToLine "toto" "test" emptyTrie;;
+let test = checkAddTrie "teto" "test" emptyTrie;;
 
-printStr (getValue (findInLine "toto" test))
+let test = checkAddTrie "tata" "yep" test;;
+
+let test = checkAddTrie "tooooo" "non" test;;
+
+let test = checkAddTrie "null" "" test;;
+(*
+addToLine test [];;
+*)
+printStr ((findInLine "toto" test));;
+print_newline ();;
+printTrie ' ' test;;
+
+(* test of tmap, exemple of a function needed for tmap *)
+let uppercaseValue value = match value with
+	| Null -> Null
+	| Value a -> Value ((String.uppercase_ascii a))
+;;
+
+printTrie ' ' (tmap uppercaseValue test);;
+
+
+let linesList = addLine test [];;
+
+
+
 
 
 (* todo : complete *)
